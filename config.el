75e7c5c4203d1ee05a9e480c383849f0596b3a79
Alex Egger <alex.egger96@gmail.com>
2019-09-30 08:08:32 +0200

    Add Org-Mode settings to config.el

--- config.el
+++ config.el
@@ -13,3 +13,22 @@
 ;; Pretty Code
 ;; Disable pretty-code in Python mode
 (setq +pretty-code-enabled-modes '(not python-mode))
+
+;; Org-Mode
+(setq org-todo-keywords '(
+                          (sequence "TODO(t)" "|" "DONE(d)")
+                          (sequence "NEXT(n)" "WAIT(w)" "HOLD(h)" "|" "ABRT(c)")
+                          (sequence "[ ](T)" "[-](P)" "[?](M)" "|" "[X](D)")
+                          )
+      org-refile-targets '(("~/org/projects.org" :maxlevel . 1)
+                           ("~/org/someday.org" :maxlevel . 1)
+                           )
+      org-capture-templates
+      '(
+        ("t" "Todos")
+        ("tt" "Todo" entry (file "~/org/inbox.org") "* TODO %?\n")
+        ("tt" "Todo with Reference" entry (file "~/org/inbox.org") "* TODO %? %^G\n%a" :empty-lines 1)
+        )
+      org-agenda-start-on-weekday 1
+      org-ellipsis " ▼ "
+      )
