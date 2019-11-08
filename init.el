e96ad78f2bd17508d316297be4709f6e6fbdec53
Alex Egger <alex.egger@mixed-mode.de>
2019-11-08 08:21:01 +0100

    Add some agenda files

--- init.el
+++ init.el
@@ -179,3 +179,17 @@
        ;; provides a Spacemacs-inspired keybinding scheme and a smartparens
        ;; config. Use it as a reference for your own modules.
        (default +bindings +smartparens))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-agenda-files
+   (quote
+    ("/home/alex/org/habits.org" "/home/alex/org/inbox.org" "/home/alex/org/journal.org" "/home/alex/org/projects.org" "/home/alex/org/someday.org" "/home/alex/org/work-cal.org"))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
