fcdf2e320a349575a59767617b28339d041ab324
Alex Egger <alex.egger@mixed-mode.de>
2019-12-05 15:39:52 +0100

    Enable ledger module

--- init.el
+++ init.el
@@ -122,7 +122,7 @@
        ;;kotlin            ; a better, slicker Java(Script)
        ;;latex             ; writing papers in Emacs has never been so fun
        ;;lean
-       ;;ledger            ; an accounting system in Emacs
+       ledger            ; an accounting system in Emacs
        ;;lua               ; one-based indices? one-based indices
        markdown          ; writing docs for people to ignore
        ;;nim               ; python + lisp at the speed of c
